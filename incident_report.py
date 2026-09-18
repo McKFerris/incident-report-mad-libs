8bb2c2c945c4bfc263aab375679b4cae1943c669
McKFerris <ferris.mck@gmail.com>
2026-09-18 10:12:13 -0500

    Add incident response details

---
+++ incident_report.py
@@ -0,0 +1,29 @@
+import random
+
+
+print("=" * 40)
+print("       CYBER INCIDENT REPORT")
+print("=" * 40)
+
+time = input("Enter a time: ")
+device = input("Enter a device (include a or an): ")
+location = input("Enter a location (include the or a): ")
+suspicious_item = input("Enter a suspicious object (include a or an): ")
+severity = input("Enter a dramatic severity level: ")
+recommended_action = input("Enter a recommended action: ")
+
+incident_summaries = [
+    f"At {time}, {device} in {location} attempted to communicate with {suspicious_item}.",
+    f"At {time}, staff in {location} discovered {suspicious_item} controlling {device}.",
+    f"Security cameras in {location} recorded {device} following {suspicious_item} at {time}.",
+    f"A report from {location} claims that {suspicious_item} tried to log in to {device} at {time}.",
+    f" Welcome to the {time} news report, we discovered that {suspicious_item} has been hired at {location}.",
+]
+
+summary = random.choice(incident_summaries)
+
+print()
+print("INCIDENT SUMMARY")
+print(summary)
+print(f"Severity: {severity}")
+print(f"Recommended Action: {recommended_action}")
